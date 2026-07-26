77deb414c8b9da404972b1bec5ade92bbb9b07b0
sahind01 <164308037+sahind01@users.noreply.github.com>
2026-07-26 20:33:41 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,26 +3,13 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'mutluads.vercel.app'],
-    },
-  },
   async headers() {
     return [
       {
-        source: '/api/serve/:path*',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-      {
-        source: '/api/track-click',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
         ],
       },
